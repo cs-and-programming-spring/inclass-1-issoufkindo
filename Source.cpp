99c7bf85a79499c665282025fb6d88623b62e0ea
kindoi67 <kindoi67@RobiLab325-10>
2019-01-22 19:34:12 -0500

    change the email

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,8 @@
 
 
 /***********************************************
-* Name: Takeshwari Kamal
-* Email: kamalt4@students.rowan.edu
+* Name: issouf kindo
+* Email: kindoi67@students.rowan.edu
 ***********************************************
 * Purpose:
 *  This program calculates area of circle
